21f9c16d90fc9630976ed08e4dd3fd6f09f63187
fachri92 <64620161+fachri92@users.noreply.github.com>
2023-03-03 18:26:03 +0700

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,13 @@
+s = 'Hacktiv8-PTP Python for Data Science'
+a = [100,200,300]
+
+def foo(arg):
+    print('arg = ' + arg)
+
+class Foo:
+    pass
+
+if (__name__=='__main__'):
+    print('Hello World')
+    print('I am Fachri')
+    foo('This is an argument')
